56fbc1877c77dbeeef4a475951bf721a4160fa0e
Ahsan Khan <nobody.ahsan@gmail.com>
2022-05-23 00:50:31 +0600

    added addBalance method

--- src/app/main/Operations.java
+++ src/app/main/Operations.java
@@ -3,16 +3,35 @@ package app.main;
 import app.classes.Cart;
 import app.classes.Product;
 
+import java.io.IOException;
+
 public class Operations {
-    public static void addBalance(double amount) {
+    public static String addBalance(double amount) {
+        String msg = "SUCCESS";
+        if (amount < 0)
+            return "Invalid Amount!";
 
+        try {
+            Main.sendObj.writeObject("addBalance");
+            Main.sendObj.writeObject(Main.user);
+            Main.sendObj.writeObject(amount);
+            String res  =  (String) Main.receiveObj.readObject();
+            if (res.equalsIgnoreCase("SUCCESS")) {
+                Main.user.addBalance(amount);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return msg;
     }
 
-    public static void addToCart(Product p) {
+    public static String addToCart(Product p) {
         // add to Main.cart as well
+        return "";
     }
 
-    public static void buy(Cart cart) {
+    public static String buy(Cart cart) {
         // add date when buy product
+        return "";
     }
 }
