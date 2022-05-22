51d69c96fa8897b65c85997a890b1614fd1f1dae
Ahsan Khan <nobody.ahsan@gmail.com>
2022-05-22 19:56:49 +0600

    moved files

--- src/app/main/Config.java
+++ src/app/main/Config.java
@@ -4,7 +4,7 @@ public class Config {
     // App Info
     public static String title = "SuperShop";
     public static String version = "v1.0";
-    public static String icon = "/res/logo.png";
+    public static String icon = "/res/img/logo.png";
     public static String userTempData = "temp.ser";
     public static double defaultHeight = 460;
     public static double defaultWeight = 750;
@@ -16,15 +16,15 @@ public class Config {
     public static String userDB = "usersDB.ser";
 
     // Pages
-    public static String homePage = "/res/Home.fxml";
-    public static String profilePage = "/res/Profile.fxml";
-    public static String cartPage = "/res/Cart.fxml";
-    public static String shopPage = "/res/Shop.fxml";
-    public static String aboutPage = "/res/About.fxml";
-    public static String historyPage = "/res/History.fxml";
-    public static String errorPage = "/res/Error.fxml";
-    public static String dashboardPage = "/res/Dashboard.fxml";
-    public static String signInPage = "/res/SignIn.fxml";
-    public static String registrationPage = "/res/Registration.fxml";
-    public static String adminHomePage = "/res/AdminHome.fxml";
+    public static String homePage = "/res/fxml/Home.fxml";
+    public static String profilePage = "/res/fxml/Profile.fxml";
+    public static String cartPage = "/res/fxml/Cart.fxml";
+    public static String shopPage = "/res/fxml/Shop.fxml";
+    public static String aboutPage = "/res/fxml/About.fxml";
+    public static String historyPage = "/res/fxml/History.fxml";
+    public static String errorPage = "/res/fxml/Error.fxml";
+    public static String dashboardPage = "/res/fxml/Dashboard.fxml";
+    public static String signInPage = "/res/fxml/SignIn.fxml";
+    public static String registrationPage = "/res/fxml/Registration.fxml";
+    public static String adminHomePage = "/res/fxml/AdminHome.fxml";
 }
