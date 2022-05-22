d31307bc6a03cd208462c9da33bdba52a90cc1a4
Ahsan Khan <nobody.ahsan@gmail.com>
2022-05-22 22:03:52 +0600

    added various dummy operations

--- src/app/main/Operations.java
+++ src/app/main/Operations.java
@@ -1,5 +1,6 @@
 package app.main;
 
+import app.classes.Cart;
 import app.classes.Product;
 
 public class Operations {
@@ -8,6 +9,10 @@ public class Operations {
     }
 
     public static void addToCart(Product p) {
+        // add to Main.cart as well
+    }
 
+    public static void buy(Cart cart) {
+        // add date when buy product
     }
 }
